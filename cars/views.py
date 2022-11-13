a04c074fa93cf3513fea6436dcc6b4c6de2b882a
Mara Green <maragreen42@gmail.com>
2022-11-13 15:08:03 -0800

    Created new view function for getting cars by id + added new urs path for car_detaile view function

--- cars/views.py
+++ cars/views.py
@@ -12,13 +12,26 @@ def cars_list(request):
         cars = Car.objects.all()
         serializer = CarSerilalizer(cars, many = True)
         return Response(serializer.data)
+
+
     elif request.method == 'POST':
         serializer = CarSerilalizer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+        serializer = CarSerilalizer(car)
+        return Response(serializer.data)
+
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    
+    
     
     
 
