55c1c505f1fee614d071dad17b6e795f3c5d4b49
Mara Green <maragreen42@gmail.com>
2022-11-13 14:23:32 -0800

    started admin server to add cars to API

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Car
+from . serializers import CarSerilalizer
 # Create your views here.
 
 @api_view(['GET'])
 def cars_list(request):
+    cars = Car.objects.all()
 
-    return Response('ok')
+    serializer = CarSerilalizer(cars, many = True)
+
+    
+
+    return Response(serializer.data)
